3c888c072cbd7189357307645fa9092de749a7cc
alswhd308 <alswhd308@gmail.com>
2019-04-18 14:03:36 +0900

    add

---
+++ src/main/java/com/alswhd308/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.alswhd308.calculator;
+import org.springframework.stereotype.Service;
+/**
+* yhthytryh
+*/
+@Service
+public class Calculator {
+	int sum(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/main/java/com/alswhd308/calculator/CalculatorApplication.java
@@ -0,0 +1,16 @@
+package com.alswhd308.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+* yhthytryh
+*/
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
